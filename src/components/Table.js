10d43a3bb0de608603b7561cdb31604180ba4f7d
Batuhan <batuhanors98@gmail.com>
2022-08-16 17:41:38 +0300

    adding daily diff

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,8 +3,34 @@ import React from "react";
 import "./Table.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "react-bootstrap/Table";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 function UserList(props) {
+  //Displaying daily diff fun
+  function DailyDiff(props) {
+    if (props.diff === 0) {
+      return <td className="daily-diff"> - </td>;
+    } else if (props.diff > 0) {
+      //rank up, green
+      return (
+        <td style={{ color: "#71C562" }} className="daily-diff">
+          {<FontAwesomeIcon icon={faAngleUp} />} {"     "}
+          {props.diff}{" "}
+        </td>
+      );
+    }
+    //rank down, red
+    else
+      return (
+        <td style={{ color: "#A7171A" }} className="daily-diff">
+          {<FontAwesomeIcon icon={faAngleDown} />} {"     "}
+          {props.diff}{" "}
+        </td>
+      );
+  }
+
   return (
     <Table striped hover variant="dark">
       <thead>
@@ -23,7 +49,7 @@ function UserList(props) {
             <td>{player.username}</td>
             <td>{player.money}</td>
             <td>{player.country}</td>
-            <td> - </td>
+            <DailyDiff diff={player.dailyDiff}></DailyDiff>
           </tr>
         ))}
       </tbody>
